a0d0d831e6b23374eae4f91fada2967fe9566406
Nguyenmanh2005 <nguyencongmanh1201@gmail.com>
2024-11-21 21:02:05 +0700

    Add files via upload

---
+++ baithuchanhjs.js
@@ -0,0 +1,15 @@
+function hienthidanhsachlop() {
+    const danhsachlop = [
+        { hoTen: "Nguyễn Công Mạnh", gioiTinh: "Nam", noiSinh: "Hà Tĩnh" },
+        { hoTen: "Trần Thị Thu Huyền", gioiTinh: "Nữ", noiSinh: "Hà Nội" },
+        { hoTen: "Nguyễn Quang Huy", gioiTinh: "Nam", noiSinh: "Hà Tĩnh" },
+        { hoTen: "Trần Đình Hưng Thịnh", gioiTinh: "Nam", noiSinh: "Nghệ An" },
+        { hoTen: "Trần Minh Long", gioiTinh: "Nam", noiSinh: "Nam Định" },
+        { hoTen: "Đào Thế Vũ", gioiTinh: "Nam", noiSinh: "Bắc Ninh" },
+        { hoTen: "Nguyễn Nam Khánh", gioiTinh: "Nam", noiSinh: "Nghệ An" },
+        { hoTen: "Nguyễn Việt Dương", gioiTinh: "Nam", noiSinh: "Hà Nội" },
+        { hoTen: "Đõ Thị Lan Hương", gioiTinh: "Nữ", noiSinh: "Hải Phòng" },
+    ];
+    console.log ('danhsach',danhsachlop)
+}
+hienthidanhsachlop();
